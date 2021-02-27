884f4b62b044d35f0861ebbcc9c69f4be2470703
farukkaydogan <faruktayyibaydogan@gmail.com>
2021-02-27 15:03:57 +0300

    27_february_2021

---
+++ 25_12_2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _25_12_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delegeler dlg = new delegeler(6,5);
+
+        }
+    }
+}
